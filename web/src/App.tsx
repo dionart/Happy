3a0069bb6fcd5871b1d351fef5fd3bf74abfcd89
arthur.dionizio <arthuroliveira@eng.ci.ufpb.br>
2020-10-27 13:43:41 -0300

    web updates

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import './styles/global.css';
-import Routes from './routes';
+import "./styles/global.css";
+import Routes from "./routes";
+import "leaflet/dist/leaflet.css";
 
 function App() {
-  return (
-    <Routes />
-  );
+  return <Routes />;
 }
 
 export default App;
--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,43 +1,57 @@
-import React from 'react';
+import React from "react";
 
-import mapMarkerImg from '../images/map-marker.svg';
-import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi'
-import { Map, TileLayer } from 'react-leaflet';
+import mapMarkerImg from "../images/map-marker.svg";
+import { Link } from "react-router-dom";
+import { FiPlus, FiArrowRight } from "react-icons/fi";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
-import 'leaflet/dist/leaflet.css';
+import "../styles/pages/orphanages-map.css";
+import mapIcon from "../utils/mapIcon";
 
-import '../styles/pages/orphanages-map.css';
+function OrphanagesMap() {
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita =D</p>
+        </header>
+        <footer>
+          <strong>Paraíba</strong>
+          <span>João Pessoa</span>
+        </footer>
+      </aside>
 
-function OrphanagesMap(){
-    return(
-        <div id="page-map">
-            <aside>
-                <header>
-                    <img src={mapMarkerImg} alt="Happy"/>
-                    <h2>Escolha um orfanato no mapa</h2>
-                    <p>Muitas crinaças estão esperando a sua visita =D</p>
-                </header>
-                <footer>
-                    <strong>Paraíba</strong>
-                    <span>João Pessoa</span>
-                </footer>
-            </aside>
+      <Map
+        center={[-7.1316393, -34.8822064]}
+        zoom={15}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
 
-            <Map
-                center={[-7.1316393,-34.8822064]}
-                zoom={15}
-                style={{width:'100%',height:'100%'}}
-
-            >
-                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
-            </Map>
-
-            <Link to="" className="create-orphanage">
-                <FiPlus size={32} color="#fff"/>
+        <Marker position={[-7.1316393, -34.8822064]} icon={mapIcon}>
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup"
+          >
+            Lar das meninas
+            <Link to="/orphanages/1">
+              <FiArrowRight size={28} color="#fFF" />
             </Link>
-        </div>
-    );
+          </Popup>
+        </Marker>
+      </Map>
+
+      <Link to="/orphanages/create" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+    </div>
+  );
 }
 
 export default OrphanagesMap;
